7511c3fb7ade174fc55aacb9ec679528d2134b78
nevoHimm <88653194+nevoHimm@users.noreply.github.com>
2021-08-09 11:34:20 +0300

    uploading project done for Weizmann Institute

---
+++ ControlYourComputer/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace MailCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.econtrol = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // econtrol
+            // 
+            this.econtrol.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.econtrol.BalloonTipText = "Hello";
+            this.econtrol.BalloonTipTitle = "There";
+            this.econtrol.Icon = ((System.Drawing.Icon)(resources.GetObject("econtrol.Icon")));
+            this.econtrol.Text = "notifyIcon1";
+            this.econtrol.Visible = true;
+            this.econtrol.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.econtrol_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Click \'Start\' to start checking emails for commands";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = " and \'Stop\' to stop it";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 322);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon econtrol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ControlYourComputer/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+using System.Configuration;
+using System.IO;
+using System.IO.Ports;
+
+namespace MailCheck
+{
+
+    public partial class Form1 : Form
+    {
+        bool doUpdate;
+        public Form1()
+        {
+            InitializeComponent();
+            doUpdate = false;
+            button1.Text = "Start";
+            Thread oThread = new Thread(new ThreadStart(doThings));
+            oThread.Start();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, System.EventArgs e)
+        {
+            Show();
+            
+            WindowState = FormWindowState.Normal;
+        }
+
+
+        private static string CheckMail(string USER, string PASS)
+        {
+            string result = "";
+            
+            try
+            {
+                var encoded = TextToBase64(USER + ":" + PASS);
+                var myWebRequest = HttpWebRequest.Create("https://gmail.google.com/gmail/feed/atom");
+                myWebRequest.Method = "POST";
+                myWebRequest.ContentLength = 0;
+                myWebRequest.Headers.Add("Authorization", "Basic " + encoded);
+
+                var response = myWebRequest.GetResponse();
+                var stream = response.GetResponseStream();
+
+                StreamReader reader = new StreamReader(stream);
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    result += line;
+                    result += "\n";
+                }
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee.Message);
+            }
+            return result;
+        }
+
+        public static string TextToBase64(string sAscii)
+        {
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+            byte[] bytes = encoding.GetBytes(sAscii);
+            return System.Convert.ToBase64String(bytes, 0, bytes.Length);
+        }
+
+        public void doThings()
+        {
+            while (true)
+            {
+                if (doUpdate)
+                {
+                    string user = "nevo.himm@gmail.com";
+                    string pass = "19981998n";
+                    try
+                    {
+                        while (true)
+                        {
+                            if (doUpdate)
+                            {
+                                SerialPort port = new SerialPort("COM5", 9600, Parity.None, 8, StopBits.One);
+                                port.Open();
+                                string dt = "";
+                                if (DateTime.Now.ToString("HH:") == "22:" || DateTime.Now.ToString("HH:") == "23:")
+                                    dt = DateTime.Now.ToString("yyyy-MM-dd") + "T" + DateTime.Now.AddHours(-3).ToString("HH:mm");
+                                if (DateTime.Now.ToString("HH:") == "00:")
+                                    dt = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd") + "T" + DateTime.Now.AddHours(-3).ToString("HH:mm");
+                                else
+                                    dt = DateTime.Now.ToString("yyyy-MM-dd") + "T" + DateTime.Now.AddHours(-3).ToString("HH:mm");
+                                string content = CheckMail(user, pass);
+                                string sender = content;
+
+                                sender = sender.Remove(content.IndexOf("</email>"), sender.Length - content.IndexOf("</email>"));
+                                sender = sender.Remove(0, content.IndexOf("<email>") + 7);
+                                if (content.IndexOf("<entry>") > 0)
+                                    content = content.Remove(0, content.IndexOf("<entry>"));
+                                else
+                                    content = "";
+
+                                if (content.IndexOf(dt) > 0 && sender == user)
+                                {
+                                    content = content.Remove(content.IndexOf("</title>"), content.Length - content.IndexOf("</title>"));
+                                    if (content.IndexOf("Get ") > 0)
+                                        content = content.Remove(0, content.IndexOf("Get "));
+                                    else
+                                    {
+                                        if (content.IndexOf("Dir ") > 0)
+                                            content = content.Remove(0, content.IndexOf("Dir "));
+
+                                        else
+                                            content = "Not found";
+                                    }
+                                    string inBox = "";
+                                    System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage("nevo.himm@gmail.com", "nevo.himm@gmail.com");
+                                    mail.Subject = "";
+                                    string str = content.Remove(0, 4);
+
+                                    if (content.IndexOf("Get") == 0 || content.IndexOf("Dir") == 0)
+                                    {
+                                        content = content.Remove(3, content.Length - 3);
+
+                                        if (content == "Get")
+                                        {
+                                            if (File.Exists(str))
+                                            {
+                                                label1.Text += "\n" + ("Getting " + str + " Time: " + DateTime.Now.ToString("HH:mm:ss"));
+                                                Attachment attachment = new Attachment(@str);
+                                                mail.Attachments.Add(attachment);
+                                                mail.Subject = str;
+                                                inBox = "There you go!";
+                                            }
+                                            else
+                                            {
+                                                inBox = "Please try again";
+                                                mail.Subject = "Could not find file";
+                                            }
+                                        }
+
+                                        else
+                                        {
+                                            if (Directory.Exists(str))
+                                            {
+                                                label1.Text += "\n" + ("Directing " + str + " Time: " + DateTime.Now.ToString("HH:mm:ss"));
+                                                mail.Subject = "the folders' content you asked for";
+                                                inBox = "The files inside that folder are:" + "\n";
+                                                string[] filePaths = Directory.GetFiles(@str);
+                                                foreach (string name in filePaths)
+                                                {
+                                                    inBox += (name + "\n");
+                                                }
+                                            }
+                                            else
+                                            {
+                                                mail.Subject = "Please try again";
+                                                inBox = "Could not find path";
+                                            }
+                                        }
+                                    }
+                                    mail.Body = inBox;
+                                    mail.IsBodyHtml = true;
+                                    SmtpClient client = new SmtpClient("smtp.gmail.com");
+                                    NetworkCredential cred = new NetworkCredential("nevo.himm@gmail.com", "19981998n");
+                                    client.EnableSsl = true;
+                                    client.Credentials = cred;
+                                    if (inBox.Length > 0)
+                                    {
+                                        try
+                                        {
+                                            client.Send(mail);
+
+                                        }
+                                        catch (Exception e)
+                                        {
+                                            Console.WriteLine(e);
+                                        }
+                                    }
+                                }
+                                port.Close();
+                                //System.Threading.Thread.Sleep(20000);
+                            }
+                        }
+                    }
+                    catch (Exception ee)
+                    {
+                        Console.WriteLine(ee.Message);
+                    }
+                }
+            }
+        }
+    
+            private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+            }
+
+        private void econtrol_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (doUpdate)
+                button1.Text = "Start";
+            else
+                button1.Text = "Stop";
+            doUpdate = !doUpdate;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
